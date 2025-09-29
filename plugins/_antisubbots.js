ffe96c82100952d49e9ae19bf30f144723125da3
yosue891 <yosueortega630@gmail.com>
2025-09-28 20:22:29 -0400

    Update _antisubbots.js

--- plugins/_antisubbots.js
+++ plugins/_antisubbots.js
@@ -1,27 +1,26 @@
-import { areJidsSameUser} from '@whiskeysockets/baileys'
-
-export async function before(m, { participants, conn}) {
+import { areJidsSameUser } from '@whiskeysockets/baileys'
+export async function before(m, { participants, conn }) {
     if (m.isGroup) {
         let chat = global.db.data.chats[m.chat];
 
-        if (!chat.antiBot2) return;
+         if (!chat.antiBot2) {
+            return
+        }
 
-        let botJid = global.conn.user.jid; // JID del bot principal
 
-        if (botJid === conn.user.jid) return;
+        let botJid = global.conn.user.jid // JID del bot principal
 
-        let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id));
+       if (botJid === conn.user.jid) {
+           return
+        } else {
+           let isBotPresent = participants.some(p => areJidsSameUser(botJid, p.id))
 
-        if (isBotPresent) {
-            setTimeout(async () => {
-                await conn.reply(m.chat,
-`╭━━〔 🌌 𝐒𝐇𝐀𝐃𝐎𝐖-𝐁𝐎𝐓 🌌 〕━━╮
-┃ He detectado la presencia del bot principal.
-┃ Como miembro de las sombras, me retiro silenciosamente...
-┃ Para evitar conflictos innecesarios.
-╰━━━━━━━━━━━━━━━━━━━━━━╯`, m, fake);
-                await this.groupLeave(m.chat);
-}, 5000); // 5 segundos
-}
-}
-              }
+          if (isBotPresent) {
+                setTimeout(async () => {
+                    await conn.reply(m.chat, `《✧》En este grupo está el bot principal, el cual me saldré para no hacer spam.`, m)
+                    await this.groupLeave(m.chat)
+                }, 5000)// 5 segundos
+            }
+        }
+    }
+    }
